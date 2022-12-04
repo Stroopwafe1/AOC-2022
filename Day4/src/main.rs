8df4145e77eed4bc7a88d41e31d7be7f84132a76
Lilith <48443491+Stroopwafe1@users.noreply.github.com>
2022-12-04 20:02:37 +0100

    Day 4 a bit neater

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -14,16 +14,16 @@ fn main() {
     let mut overlaps_p2 = 0;
     for pair in contents.lines() {
         let mut pair_split = pair.split(',');
-        let mut elf1_split = pair_split.next().expect("Have value").split('-');
-        let mut elf2_split = pair_split.next().expect("Have value").split('-');
+        let (elf1_beg, elf1_end) = pair_split.next().expect("Have value").split_once('-').unwrap();
+        let (elf2_beg, elf2_end) = pair_split.next().expect("Have value").split_once('-').unwrap();
 
-        let elf1_range = elf1_split.next().expect("value").parse::<i32>().unwrap()..(elf1_split.next().expect("value").parse::<i32>().unwrap() + 1);
-        let elf2_range = elf2_split.next().expect("value").parse::<i32>().unwrap()..(elf2_split.next().expect("value").parse::<i32>().unwrap() + 1);
+        let elf1_range = elf1_beg.parse::<i32>().unwrap()..(elf1_end.parse::<i32>().unwrap() + 1);
+        let elf2_range = elf2_beg.parse::<i32>().unwrap()..(elf2_end.parse::<i32>().unwrap() + 1);
         
         let elf1: HashSet<i32> = HashSet::from_iter(elf1_range);
         let elf2: HashSet<i32> = HashSet::from_iter(elf2_range);
 
-        if (elf1.is_subset(&elf2) || elf2.is_subset(&elf1)) && !elf1.is_empty() && !elf2.is_empty() {
+        if elf1.is_subset(&elf2) || elf2.is_subset(&elf1) {
             overlaps += 1;
         }
 
