cc78797e37eff5bc5c54a038d82951829da5b568
Lilith <48443491+Stroopwafe1@users.noreply.github.com>
2022-12-09 12:58:53 +0100

    Day 9 that I can be proud of

--- Day9/src/main.rs
+++ Day9/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 
@@ -10,73 +10,52 @@ fn main() {
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    part_one(&contents);
     let len = 10;
-    let mut snake: Vec<Coord> = vec![(0, 0); len]; // Head is 0
-    let mut visited_coords: HashSet<Coord> = HashSet::new();
+    let mut snake1: Vec<Coord> = vec![(0, 0); 2]; // Head is 0
+    let mut snake2: Vec<Coord> = vec![(0, 0); len]; // Head is 0
+    let mut visited_coords1: HashSet<Coord> = HashSet::new();
+    let mut visited_coords2: HashSet<Coord> = HashSet::new();
+
     for line in contents.lines() {
         let motion: (&str, u32) = line
         .split_once(' ')
         .map(|f| (f.0, f.1.parse::<u32>().unwrap()))
         .unwrap();
-        move_dir(&mut snake, &mut visited_coords, motion.0, motion.1, len);
-        //debug_set(&visited_coords);
-        //println!("-----------{}----------------", motion.0);
+        move_dir(&mut snake1, &mut visited_coords1, motion.0, motion.1, 2);
+        move_dir(&mut snake2, &mut visited_coords2, motion.0, motion.1, len);
     }
-    println!("Part two: {}", visited_coords.len());
-    //debug(&visited_coords);
+    println!("Part one: {}", visited_coords1.len());
+    println!("Part two: {}", visited_coords2.len());
 }
 
 fn move_dir(snake: &mut Vec<Coord>, coords: &mut HashSet<Coord>, direction: &str, amount: u32, len: usize) {
-    match direction {
-        "U" => {
-            for _ in 0..amount {
-                snake.get_mut(0).unwrap().1 += 1;
-                move_shit(snake, coords, len);
-            }
-        },
-        "D" => {
-            for _ in 0..amount {
-                snake.get_mut(0).unwrap().1 -= 1;
-                move_shit(snake, coords, len);
-            }
-        },
-        "L" => {
-            for _ in 0..amount {
-                snake.get_mut(0).unwrap().0 -= 1;
-                move_shit(snake, coords, len);
-            }
-        },
-        "R" => {
-            for _ in 0..amount {
-                snake.get_mut(0).unwrap().0 += 1;
-                move_shit(snake, coords, len);
-            }
+    for _ in 0..amount {
+        match direction {
+            "U" => snake.get_mut(0).unwrap().1 += 1,
+            "D" => snake.get_mut(0).unwrap().1 -= 1,
+            "L" => snake.get_mut(0).unwrap().0 -= 1,
+            "R" => snake.get_mut(0).unwrap().0 += 1,
+            _ => unreachable!()
         }
-        _ => unreachable!()
+        move_snake(snake, coords, len);
     }
-    
 }
 
-fn move_shit(snake: &mut Vec<Coord>, coords: &mut HashSet<Coord>, len: usize) {
+fn move_snake(snake: &mut Vec<Coord>, coords: &mut HashSet<Coord>, len: usize) {
     for i in 1..len {
         let h = *snake.get_mut(i - 1).unwrap();
         let mut t = snake.get_mut(i).unwrap();
-        //println!(" H({}, {}), T({}, {})", h.0, h.1, t.0, t.1);
-        if is_diff1(h, *t) {
+        let a = h.0 - t.0;
+        let b = h.1 - t.1;
+        let c = a*a + b*b;
+
+        if c <= 2 { // Can only be 0, 1, 2
             continue;
-        } else if is_diff2(h, *t) {
-            println!("Diff2: H({}, {}), T({}, {})", h.0, h.1, t.0, t.1);
-            if t.0 < h.0 && t.1 == h.1 {
-                t.0 = h.0 - 1;
-            } else if t.0 > h.0 && t.1 == h.1 {
-                t.0 = h.0 + 1;
-            } else if t.1 < h.1 && t.0 == h.0 {
-                t.1 = h.1 - 1;
-            } else if t.1 > h.1 && t.0 == h.0 {
-                t.1 = h.1 + 1;
+        } else if c == 4 { // One space difference
+            if a == 0 {
+                t.1 = if b == -2 { h.1 + 1 } else { h.1 - 1 };
             } else {
-                unreachable!();
+                t.0 = if a == -2 { h.0 + 1 } else { h.0 - 1 };
             }
         } else {
             // Move diagonally
@@ -93,128 +72,5 @@ fn move_shit(snake: &mut Vec<Coord>, coords: &mut HashSet<Coord>, len: usize) {
         }
     }
     let last = *snake.last_mut().unwrap();
-    println!("Snake: {:?})", snake);
     coords.insert(last);
-    //println!("--------------------------");
-}
-
-fn is_diff1(h: Coord, t: Coord) -> bool {
-    let a = h.0 - t.0;
-    let b = h.1 - t.1;
-    let c = a*a + b*b;
-    return c == 0 || c == 1 || c == 2;
-}
-
-fn is_diff2(h: Coord, t: Coord) -> bool {
-    let a = h.0 - t.0;
-    let b = h.1 - t.1;
-    let c = a*a + b*b;
-    return c == 4;
-}
-
-fn part_one(contents: &String) {
-    let mut h_coord: Coord = (0, 0);
-    let mut t_coord: Coord = (0, 0);
-    let mut visited_coords: HashMap<Coord, u32> = HashMap::new();
-    visited_coords.insert(t_coord, 1);
-    for line in contents.lines() {
-        let motion: (&str, u32) = line
-            .split_once(' ')
-            .map(|f| (f.0, f.1.parse::<u32>().unwrap()))
-            .unwrap();
-        match motion.0 {
-            "U" => {
-                for _ in 0..motion.1 {
-                    h_coord.1 += 1;
-                    if !is_diff1(h_coord, t_coord) {
-                        t_coord.0 = h_coord.0;
-                        t_coord.1 = h_coord.1 - 1;
-                        if visited_coords.contains_key(&t_coord) {
-                            *visited_coords.get_mut(&t_coord).unwrap() += 1;
-                        } else {
-                            visited_coords.insert(t_coord, 1);
-                        }
-                    }
-                }
-            },
-            "D" => {
-                for _ in 0..motion.1 {
-                    h_coord.1 -= 1;
-                    if !is_diff1(h_coord, t_coord) {
-                        t_coord.0 = h_coord.0;
-                        t_coord.1 = h_coord.1 + 1;
-                        if visited_coords.contains_key(&t_coord) {
-                            *visited_coords.get_mut(&t_coord).unwrap() += 1;
-                        } else {
-                            visited_coords.insert(t_coord, 1);
-                        }
-                    }
-                }
-            },
-            "L" => {
-                for _ in 0..motion.1 {
-                    h_coord.0 -= 1;
-                    if !is_diff1(h_coord, t_coord) {
-                        t_coord.0 = h_coord.0 + 1;
-                        t_coord.1 = h_coord.1;
-                        if visited_coords.contains_key(&t_coord) {
-                            *visited_coords.get_mut(&t_coord).unwrap() += 1;
-                        } else {
-                            visited_coords.insert(t_coord, 1);
-                        }
-                    }
-                }
-            },
-            "R" => {
-                for _ in 0..motion.1 {
-                    h_coord.0 += 1;
-                    if !is_diff1(h_coord, t_coord) {
-                        t_coord.0 = h_coord.0 - 1;
-                        t_coord.1 = h_coord.1;
-                        if visited_coords.contains_key(&t_coord) {
-                            *visited_coords.get_mut(&t_coord).unwrap() += 1;
-                        } else {
-                            visited_coords.insert(t_coord, 1);
-                        }
-                    }
-                }
-            },
-            _ => unreachable!()
-        }
-        //println!("Curr t_coord: ({}, {})", t_coord.0, t_coord.1);
-    }
-
-    println!("Part one: {}", visited_coords.keys().len());
-}
-
-fn debug(map: &HashMap<Coord, u32>) {
-    let max_x = map.keys().map(|c| c.0).max().unwrap();
-    let max_y = map.keys().map(|c| c.1).max().unwrap();
-    for y in 0..=max_y {
-        for x in 0..=max_x {
-            if map.contains_key(&(x, y)) {
-                print!("{}", *map.get(&(x, y)).unwrap());
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-}
-
-fn debug_set(map: &HashSet<Coord>) {
-    let max_x = map.into_iter().map(|c| c.0).max().unwrap();
-    let max_y = map.into_iter().map(|c| c.1).max().unwrap();
-    let min_x = map.into_iter().map(|c| c.0).min().unwrap();
-    let min_y = map.into_iter().map(|c| c.1).min().unwrap();
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            if map.contains(&(x, y)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
 }
